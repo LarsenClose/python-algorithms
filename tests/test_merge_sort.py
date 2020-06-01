fb0422e2bddc49d575d39426314e7c5eb16795a7
Larsen Close <LarsenClose@protonmail.com>
2020-06-01 03:46:47 -0600

    clean up

--- tests/test_merge_sort.py
+++ tests/test_merge_sort.py
@@ -26,5 +26,3 @@ class TestMergeSort(unittest.TestCase):
     def test_sorted(self):
         self.assertEqual(merge_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
                          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-
